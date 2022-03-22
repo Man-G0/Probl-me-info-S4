72e68a0c206204a187358420acb79a3be79b3cef
Manon GOFFINET <manon.goffinet@edu.devinci.fr>
2022-03-22 17:23:56 +0100

    rotation et TD5

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ namespace Manon_Aubry_Manon_Goffinet
         {
 
 
-
+            
             string fileN = "coco.bmp";
             MyImage image = new MyImage(fileN);
             //image1.From_Image_To_File("sortie.bmp");
@@ -38,17 +38,17 @@ namespace Manon_Aubry_Manon_Goffinet
             //MyImage imageReduire = image.Réduire();
             //imageReduire.From_Image_To_File("sortie7.bmp");
 
-            //MyImage imageFlou = image.Flou();
-            //imageFlou.From_Image_To_File("sortie-Flou.bmp");
+            MyImage imageFlou = image.Flou();
+            imageFlou.From_Image_To_File("sortie-Flou.bmp");
 
-            //MyImage imageRepoussage = image.Repoussage();
-            //imageRepoussage.From_Image_To_File("sortie-Repoussage.bmp");
+            MyImage imageRepoussage = image.Repoussage();
+            imageRepoussage.From_Image_To_File("sortie-Repoussage.bmp");
 
-            //MyImage imagedétectionDesBords = image.DétectionDesBords();
-            //imagedétectionDesBords.From_Image_To_File("sortie-DétectionDesBords.bmp");
+            MyImage imagedétectionDesBords = image.DétectionDesBords();
+            imagedétectionDesBords.From_Image_To_File("sortie-DétectionDesBords.bmp");
 
-            //MyImage imageRenforcementDesBords = image.RenforcementDesBords();
-            //imageRenforcementDesBords.From_Image_To_File("sortie-RenforcementDesBords.bmp");
+            MyImage imageRenforcementDesBords = image.RenforcementDesBords();
+            imageRenforcementDesBords.From_Image_To_File("sortie-RenforcementDesBords.bmp");
 
             Console.ReadKey();
         }
